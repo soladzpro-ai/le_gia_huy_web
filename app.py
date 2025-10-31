15f14f608a19e671c500032de2ef6fa49f9b7f63
SLDZPRO <sldzpro@example.com>
2025-10-31 20:37:59 +0700

    First commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/name')
+def name():
+    return render_template('name.html')
+
+@app.route('/age')
+def age():
+    return render_template('age.html')
+
+@app.route('/school')
+def school():
+    return render_template('school.html')
+
+@app.route('/hobby')
+def hobby():
+    return render_template('hobby.html')
+
+@app.route('/countdown')
+def countdown():
+    return render_template('countdown.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
